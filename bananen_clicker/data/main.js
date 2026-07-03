d89032f93e98a726fb1c0bcd9a75633925cb4bdd
JanJansenOhneK <janjansenohnek@gmail.com>
2026-07-03 15:44:32 +0200

    bananenclicker keks

--- bananen_clicker/data/main.js
+++ bananen_clicker/data/main.js
@@ -1,18 +1,66 @@
 
+// von janjansen
+// gemacht mit meinen eigenen händen
+// nicht von ki
+
 const stylesheet = document.getElementById("stylesheet").sheet
 const button = document.getElementById("button")
 const scoretext = document.getElementById("score")
 const bpctext = document.getElementById("bpc")
 const bpstext = document.getElementById("bps")
+const cookietext = document.getElementById("cookie-text")
 const storediv = document.getElementsByClassName("storediv")
 let score = 0
 let click_modifier = 1
 let bps = 0
 
+function deleteCookie() {
+    document.cookie = "username=;"
+}
+
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";";
+}
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function checkCookie() {
+    let username = getCookie("username");
+    if (username != "") {
+        console.log("user ist zurück");
+        score = Number(getCookie("bananen"));
+        click_modifier = Number(getCookie("bpc"));
+        bps = Number(getCookie("bps"));
+    } else {
+        console.log("neuer user");
+        setCookie("username","user");
+        setCookie("bananen","0");
+        setCookie("bpc","1");
+        setCookie("bps","0");
+    }
+}
+
+checkCookie()
+
 function loop() {
-    scoretext.textContent = score
-    bpctext.textContent = click_modifier.toString() + " Bananen pro Click"
-    bpstext.textContent = bps.toString() + " Bananen pro Sekunde"
+    scoretext.textContent = score.toString();
+    //cookietext.textContent = "cookie: " + document.cookie.toString();
+    bpctext.textContent = click_modifier.toString() + " Bananen pro Click";
+    bpstext.textContent = bps.toString() + " Bananen pro Sekunde";
+    setCookie("bananen",score)
+    setCookie("bpc",click_modifier)
+    setCookie("bps",bps)
 }
 function clicked() {
     score += click_modifier
